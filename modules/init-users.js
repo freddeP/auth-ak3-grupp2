465c67100a0856643de7ce96f9f44ef0dc4203e5
Fredric Persson <freper7912@skynet.halmstad.se>
2019-11-29 10:14:37 +0100

    fix double user in init-user

--- modules/init-users.js
+++ modules/init-users.js
@@ -1,16 +1,24 @@
 const bcrypt = require("bcryptjs");
 
-let email = "john@bengtsson.se";
+let email = "fredde@p.se";
 let password = "blackfriday";
 
 let hash = bcrypt.hashSync(password,12);
 
 let users = require("fs").readFileSync(__dirname+"/users.json").toString();
 users = JSON.parse(users);
+// kolla så att användare inte existerar redan
+let userExists = users.filter(u=>{
+    return u.email == email;
+});
 
-users.push({email:email,password:hash});
+if(userExists.length==0)
+{
+    users.push({email:email,password:hash});
 
-users = JSON.stringify(users);
+    users = JSON.stringify(users);
 
-require("fs").writeFileSync(__dirname+"/users.json",users);
-
+    require("fs").writeFileSync(__dirname+"/users.json",users);
+    console.log(users);
+}
+else {console.log("user exists")}
